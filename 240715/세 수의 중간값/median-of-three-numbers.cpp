e36d396e17d18d0b9b1ee6059830e04671da0c46
Junhyuk <62590913+JunhyukBaik@users.noreply.github.com>
2024-07-15 17:31:20 +0900

    Title: 세 수의 중간값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240715/세 수의 중간값/median-of-three-numbers.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+
+    cin >> a >> b >> c;
+
+    if (b > a){
+        if (b < c)
+            cout << 1;
+        else
+            cout << 0;
+    }
+    else  
+        cout << 0;
+        
+    return 0;
+}
