f5614beeed1f535cd1455527aad45bf44c94b841
Junhyuk <62590913+JunhyukBaik@users.noreply.github.com>
2024-07-16 17:18:02 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240716/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
+++ 240716/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -8,7 +8,7 @@ int main() {
 
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n - i - 1; j++)
-            cout << '  ';
+            cout << "  ";
     
         for (int j = 0; j < i + 1; j++)
             cout << "@ ";
