0c81e7c63255d053db779b858455014b2d243c99
Junhyuk <62590913+JunhyukBaik@users.noreply.github.com>
2024-07-16 17:17:50 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240716/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
+++ 240716/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -8,9 +8,8 @@ int main() {
 
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n - i - 1; j++)
-            cout << ' ';
-        for (int j = 0; j < n - i - 1; j++)
-            cout << ' ';       
+            cout << '  ';
+    
         for (int j = 0; j < i + 1; j++)
             cout << "@ ";
         cout << endl;
